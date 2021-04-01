f95a4fd4dad67821462f839509e2f5c91ec640e8
Anirbannayek21 <anirbannayek2@gmail.com>
2021-04-01 21:22:13 +0530

    material icon and line through

--- src/List.js
+++ src/List.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css'
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const List = (props) => {
 
+    const [line, setline] = useState(false);
+
+    const cutit = () =>{
+        setline(true);
+    }
     
     return (
         <>
         <div className="todo_list">
-            <button className="delete" onClick={()=>{
-                props.onSelect(props.id)
-            }}>x</button>
-            <li className="listItem">{props.textValue}</li>
+            <button className="delete" onDoubleClick={()=>{
+                props.onSelect(props.id) }} onClick={cutit}
+            ><DeleteIcon style={{fontSize:"25px"}}/></button>
+            <li className="listItem"
+            style={{textDecoration: line ? "line-through":"none"}}>{props.textValue}</li>
         </div>
         </>
     )
